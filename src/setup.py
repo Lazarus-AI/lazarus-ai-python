70a8176861929d0c6ae32d72a530a31514df75fe
KK Barrows <140852522+kathleenbarrows@users.noreply.github.com>
2023-11-15 16:14:05 -0500

    Update setup.py for first release

--- src/setup.py
+++ src/setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 setup(
   name = 'lazarus-ai',
   packages = ['lazarus_ai', 'errors', 'utils'],
-  version = '0.11',
+  version = '0.01',
   license='MIT',
   description = 'Lazarus AI Python Library',
   author = 'Caroline Abel, KK Barrows, Alvin Xie',
   author_email = 'caroline@lazarus.enterprises, kathleen@lazarus.enterprises, alvin@lazarus.enterprises',
   url = 'https://www.lazarusforms.com/',
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # TODO
+  download_url = 'https://github.com/kathleenbarrows/lazarus-ai-python/archive/v_0.01.tar.gz',
   install_requires=[
           'requests',
           'stringcase',
